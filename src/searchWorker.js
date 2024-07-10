57047dd7cdba5ea501f1116dfc2b7179b1978091
dedestem <78974474+dedestem@users.noreply.github.com>
2024-07-10 07:51:43 +0200

    Scheduled deletion for searchWorker.js

--- src/searchWorker.js
+++ src/searchWorker.js
@@ -1,11 +1,2 @@
-// searchWorker.js
-self.onmessage = async function(event) {
-    const { path, query, invoke } = event.data;
-    try {
-      const result = await eval(`(${invoke})`)('search', { path, query });
-      self.postMessage({ result });
-    } catch (error) {
-      self.postMessage({ error: error.message });
-    }
-  };
+// delete this file
   
