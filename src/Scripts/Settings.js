5c8440acaddbb6cfacd757dfdfdc66bdf5295e4a
David Bos <david.bos@outlook.com>
2024-07-23 16:12:40 +0200

    Color changing ok but not transparency saving etc all working

--- src/Scripts/Settings.js
+++ src/Scripts/Settings.js
@@ -1,6 +1,9 @@
 // Initialize Tauri API
 const { invoke } = window.__TAURI__.tauri;
 
+// Modules
+import { Save, Load } from './Data.js'
+
 const backcolorInput = document.getElementById('backcolor');
 const transparencyInput = document.getElementById('transparency');
 
@@ -8,9 +11,7 @@ function updateColor() {
     const hex = backcolorInput.value;
     const alpha = transparencyInput.value;
     const rgbaColor = hexToRgba(hex, alpha);
-    invoke("toggle_decorations", { should_have_decorations: false });
     document.body.style.backgroundColor = rgbaColor;
-    invoke("toggle_decorations", { should_have_decorations: true });
     console.log(rgbaColor)
 }
 
@@ -18,10 +19,40 @@ function hexToRgba(hex, alpha) {
     const r = parseInt(hex.slice(1, 3), 16);
     const g = parseInt(hex.slice(3, 5), 16);
     const b = parseInt(hex.slice(5, 7), 16);
-    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+    savecolors(r,g,b,alpha)
+    return `rgba(${r}, ${g}, ${b}, ${1})`; // change 1 to aplha when doing transparency dev
+}
+
+function rgbToHex(r, g, b) {
+    const toHex = (n) => {
+      let hex = n.toString(16);
+      return hex.length === 1 ? "0" + hex : hex;
+    };
+    
+    return "#" + toHex(r) + toHex(g) + toHex(b);
+}
+
+function savecolors(r,g,b,a) {
+    Save(r,"rbackground")
+    Save(g,"gbackground")
+    Save(b,"bbackground")
+    Save(a,"abackground")
+}
+
+function Loadcolors() {
+    const r = Load("rbackground")
+    const g = Load("gbackground")
+    const b = Load("bbackground")
+    const a = Load("abackground")
+
+    const hex = rgbToHex(r,g,b)
+    backcolorInput.value = hex;
+    transparencyInput.value = a;
+
+    updateColor();
 }
 
 backcolorInput.addEventListener('input', updateColor);
 transparencyInput.addEventListener('input', updateColor);
 
-updateColor();
+Loadcolors();
